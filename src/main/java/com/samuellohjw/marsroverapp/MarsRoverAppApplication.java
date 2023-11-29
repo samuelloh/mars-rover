864f58a8396b80a2598dcd421e97114fbeeef3a7
Samuel Loh <samuellohjw@gmail.com>
2023-11-30 00:05:00 +0800

    First Commit

---
+++ src/main/java/com/samuellohjw/marsroverapp/MarsRoverAppApplication.java
@@ -0,0 +1,13 @@
+package com.samuellohjw.marsroverapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class MarsRoverAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverAppApplication.class, args);
+	}
+}
---
+++ src/test/java/com/samuellohjw/marsroverapp/MarsRoverAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuellohjw.marsroverapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
